9e40c441569ceda16685faa1b49b953f4741f4ed
Akemiko <tcop29711@gmail.com>
2024-05-02 23:49:41 +0800

    update themes and styles

--- src/shared/ui/Breadcrumbs/ui/Breadcrumbs.tsx
+++ src/shared/ui/Breadcrumbs/ui/Breadcrumbs.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,6 +8,17 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/shared/ui/breadcrumb";
+import { useTheme } from "next-themes";
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+
+import { Button } from "@/shared/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/shared/ui/dropdown-menu"
 
 type BreadcrumbItemType = {
   value: string;
@@ -19,6 +32,7 @@ interface BreadcrumbsProps {
 
 export const Breadcrumbs = (props: BreadcrumbsProps) => {
   const { breadcrumbs, current } = props;
+  const { setTheme } = useTheme()
 
   return (
     <Breadcrumb>
@@ -34,6 +48,28 @@ export const Breadcrumbs = (props: BreadcrumbsProps) => {
           ))}
         <BreadcrumbItem>
           <BreadcrumbPage>{current}</BreadcrumbPage>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         </BreadcrumbItem>
       </BreadcrumbList>
     </Breadcrumb>
