bffab0770f35254e56c7646dddf5d746684dd4cb
Akemiko <67152382+Arsenii1109@users.noreply.github.com>
2024-05-29 21:08:11 +0300

    GL-00: Добавлен кастомный обработчик авторизации (#37)
    * Adding jvm args for WebClient
    
    * Fix to camelCase
    
    * Adding custom auth

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,2 +1,4 @@
 export const HREF_GET_DATALIFE_ENGINE_AUTH_PHP =
   "https://raw.githubusercontent.com/GamerVII-NET/Gml.Modules.Auth/master/DLE/auth.php";
+export const HREF_DOCUMENTATION_CUSTOM_ENDPOINT =
+  "https://gamervii-net.github.io/Gml.Docs/integrations-auth-custom.html#1-gml-backend";
