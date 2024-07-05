359f7caf976757cdd4b434cc37b02edd562bd96d
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-05 12:14:23 +0300

    Добавлены новые компоненты в настройки
    В файле настроек добавлены новые UI компоненты и обновлено их размещение. Список новых компонентов включает CircleUser, Menu, Package2, Search, Sheet и другие. Это позволяет улучшить навигацию и пользовательский интерфейс на страницах настроек.

--- src/views/settings/ui/Settings.tsx
+++ src/views/settings/ui/Settings.tsx
@@ -2,6 +2,28 @@ import { EditSettingsPlatformForm } from "@/features/edit-settings-platform-form
 
 import { Breadcrumbs } from "@/shared/ui/Breadcrumbs";
 import { DASHBOARD_PAGES } from "@/shared/routes";
+import { CircleUser, Menu, Package2, Search } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/shared/ui/sheet";
+import { Button } from "@/shared/ui/button";
+import { Input } from "@/shared/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/shared/ui/dropdown-menu";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/shared/ui/card";
+import { Checkbox } from "@/shared/ui/checkbox";
+import Link from "next/link";
 
 export const SettingsPage = () => {
   return (
@@ -10,11 +32,16 @@ export const SettingsPage = () => {
         current="Настройки платформы"
         breadcrumbs={[{ value: "Главная", path: DASHBOARD_PAGES.HOME }]}
       />
-      <div className="flex flex-col items-start py-4">
-        <div className="flex justify-between w-full">
-          <h1 className="text-xl font-bold mb-8">Настройки платформы</h1>
-        </div>
-        <div className="flex flex-col gap-y-6 w-full">
+      <div className="mx-auto grid w-full gap-2">
+        <h1 className="text-3xl font-semibold">Настройки</h1>
+      </div>
+      <div className="mx-auto grid w-full items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+        <nav className="grid gap-4 text-sm text-muted-foreground" x-chunk="dashboard-04-chunk-0">
+          <Link href="#" className="font-semibold text-primary">
+            Основные
+          </Link>
+        </nav>
+        <div className="grid gap-6">
           <EditSettingsPlatformForm />
         </div>
       </div>
