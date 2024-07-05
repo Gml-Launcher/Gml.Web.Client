82c9e857bf924d5908f4bca0d22aed230cc226e5
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-05 11:13:06 +0300

    Обновлен дизайн страницы регистрации
    Дизайн страницы регистрации был обновлен. Импорт формы регистрации и входа был добавлен в страницу. Был изменен общий стиль страницы, а также добавлен логотип с помощью модуля "next/image".

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,32 +1,36 @@
 import Link from "next/link";
 
-import { SignUpForm } from "../../../features/auth-credentials-form";
+import { SignInForm, SignUpForm } from "../../../features/auth-credentials-form";
 import { cn } from "../../../shared/lib/utils";
 import { AUTH_PAGES } from "../../../shared/routes";
 import { buttonVariants } from "@/shared/ui/button";
+import Image from "next/image";
+import logo from "@/assets/logos/logo.svg";
 
 export default function SignUPPage() {
   return (
     <>
-      <div className="container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
-        <Link
-          href={AUTH_PAGES.SIGN_IN}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8",
-          )}
-        >
-          Войти
-        </Link>
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Создайте учетную запись</h1>
-            <p className="text-sm text-muted-foreground">
-              Введите свой адрес электронной почты ниже, чтобы создать свою учетную запись
-            </p>
+      <div className="w-full lg:grid lg:min-h-[100vh] lg:grid-cols-2 xl:min-h-[100vh]">
+        <div className="flex items-center justify-center py-12">
+          <div className="mx-auto grid w-[350px] gap-6">
+            <div className="grid gap-3">
+              <Image src={logo} className="w-12" alt="GML Frontend" />
+              <h1 className="text-3xl font-bold">Регистрация</h1>
+              <p className="text-balance text-muted-foreground">
+                Введите свой адрес электронной почты ниже, чтобы создать свою учетную запись
+              </p>
+            </div>
+            <SignUpForm />
+            <div className="mt-4 text-center text-sm">
+              Уже есть аккаунт?{" "}
+              <Link href={AUTH_PAGES.SIGN_IN} className="underline">
+                Войти
+              </Link>
+            </div>
           </div>
-
-          <SignUpForm />
+        </div>
+        <div className="hidden bg-muted lg:flex items-center justify-center">
+          <Image src={logo} className="w-42 opacity-10" alt="GML Frontend" />
         </div>
       </div>
     </>
