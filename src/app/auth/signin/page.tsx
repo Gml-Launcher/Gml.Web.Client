88c0980399ebcbc5bf9c80a4ef39b399a5805831
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-05 11:12:56 +0300

    Изменен дизайн страницы авторизации
    Страница входа в учетную запись была обновлена: изменено расположение элементов и заголовков, добавлены новые, также добавлены логотипы. Обновления направлены на повышение удобства использования пользователем этой страницы.

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,30 +3,37 @@ import Link from "next/link";
 import { SignInForm } from "../../../features/auth-credentials-form";
 import { cn } from "../../../shared/lib/utils";
 import { AUTH_PAGES } from "../../../shared/routes";
-import { buttonVariants } from "../../../shared/ui/button";
+import { Button, buttonVariants } from "../../../shared/ui/button";
+import { Label } from "@/shared/ui/label";
+import { Input } from "@/shared/ui/input";
+import classes from "@/app/dashboard/styles.module.css";
+import Image from "next/image";
+import logo from "@/assets/logos/logo.svg";
 
 export default function SignInPage() {
   return (
     <>
-      <div className="container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
-        <Link
-          href={AUTH_PAGES.SIGN_UP}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8",
-          )}
-        >
-          Регистрация
-        </Link>
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Войдите в учетную запись</h1>
-            <p className="text-sm text-muted-foreground">
-              Введите свой логин и пароль ниже, чтобы войти в свою учетную запись
-            </p>
+      <div className="w-full lg:grid lg:min-h-[100vh] lg:grid-cols-2 xl:min-h-[100vh]">
+        <div className="flex items-center justify-center py-12">
+          <div className="mx-auto grid w-[350px] gap-6">
+            <div className="grid gap-3">
+              <Image src={logo} className="w-12" alt="GML Frontend" />
+              <h1 className="text-3xl font-bold">Авторизация</h1>
+              <p className="text-balance text-muted-foreground">
+                Введите свой логин и пароль ниже, чтобы войти в свою учетную запись
+              </p>
+            </div>
+            <SignInForm />
+            <div className="mt-4 text-center text-sm">
+              Нет аккаунта?{" "}
+              <Link href={AUTH_PAGES.SIGN_UP} className="underline">
+                Регистрация
+              </Link>
+            </div>
           </div>
-
-          <SignInForm />
+        </div>
+        <div className="hidden bg-muted lg:flex items-center justify-center">
+          <Image src={logo} className="w-42 opacity-10" alt="GML Frontend" />
         </div>
       </div>
     </>
