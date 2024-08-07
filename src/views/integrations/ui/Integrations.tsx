24534a840eaba9fa88c6339497620ed8b2d2bf9d
akemiko <tcop29711@gmail.com>
2024-08-07 09:50:37 +0300

    Restructure Integrations page layout
    Reorganized the Integrations page to improve readability by grouping related integration cards under distinct headings. Added "Лаунчер" and "Дополнительное" sections to better categorize specific types of integrations. Updated margins and layout for improved user experience.

--- src/views/integrations/ui/Integrations.tsx
+++ src/views/integrations/ui/Integrations.tsx
@@ -30,51 +30,63 @@ export const IntegrationsPage = () => {
       />
       <div className="flex flex-col items-start py-4">
         <div className="flex justify-between w-full">
-          <h1 className="text-xl font-bold mb-8">Интеграции</h1>
+          <h1 className="text-xl font-bold mb-4">Интеграции</h1>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-          <IntegrationCard
-            title="Аутентификация"
-            description="Синхронизация и управление данными о пользователях на платформе"
-            dialog={<ChooseAuthenticationMethodDialog />}
-            testid={DATA_TEST_ID_DIALOG_AUTHENTICATION_METHOD}
-          />
-          <IntegrationCard
-            title="Сборка лаунчера"
-            description="Создайте лаунчер для платформ Windows, MacOS и Linux в пару кликов"
-            dialog={<GenerateLauncherDialog />}
-            testid={DATA_TEST_ID_DIALOG_GENERATE_LAUNCHER}
-          />
-          <IntegrationCard
-            title="Сервис скинов"
-            description="Добавь интеграцию со сервисом скинов, для отображения скинов и плащей в игре"
-            dialog={<ConnectTexturesDialog />}
-            testid={DATA_TEST_ID_DIALOG_CONNECT_TEXTURES}
-          />
-          <IntegrationCard
-            title="Sentry"
-            description={"Подключение платформы для отслеживания ошибок и мониторинга приложений"}
-            dialog={<ConnectSentryDialog />}
-            testid={DATA_TEST_ID_DIALOG_CONNECT_SENTRY}
-          />
-          <IntegrationCard
-            title="Discord"
-            description="Синхронизация лаунчера и вашего Discord сервера"
-            dialog={<ConnectDiscordDialog />}
-            testid={DATA_TEST_ID_DIALOG_CONNECT_DISCORD}
-          />
-          <IntegrationCard
-            title="Нужен сервис?"
-            description="Отправь заявку, а мы придумаем что-нибудь"
-            dialog={
-              <Link target="_blank" href={HREF_DISCORD}>
-                <Button size="sm" variant="outline" className="w-fit">
-                  <LinkIcon className="mr-2" size={16} />
-                  Поддержка
-                </Button>
-              </Link>
-            }
-          />
+        <div className="flex flex-col gap-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+            <IntegrationCard
+              title="Аутентификация"
+              description="Синхронизация и управление данными о пользователях на платформе"
+              dialog={<ChooseAuthenticationMethodDialog />}
+              testid={DATA_TEST_ID_DIALOG_AUTHENTICATION_METHOD}
+            />
+            <IntegrationCard
+              title="Сервис скинов"
+              description="Добавь интеграцию со сервисом скинов, для отображения скинов и плащей в игре"
+              dialog={<ConnectTexturesDialog />}
+              testid={DATA_TEST_ID_DIALOG_CONNECT_TEXTURES}
+            />
+            <IntegrationCard
+              title="Discord"
+              description="Синхронизация лаунчера и вашего Discord сервера"
+              dialog={<ConnectDiscordDialog />}
+              testid={DATA_TEST_ID_DIALOG_CONNECT_DISCORD}
+            />
+          </div>
+          <div className="flex justify-between w-full">
+            <h1 className="text-xl font-bold">Лаунчер</h1>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+            <IntegrationCard
+              title="Сборка лаунчера"
+              description="Создайте лаунчер для платформ Windows, MacOS и Linux в пару кликов"
+              dialog={<GenerateLauncherDialog />}
+              testid={DATA_TEST_ID_DIALOG_GENERATE_LAUNCHER}
+            />
+          </div>
+          <div className="flex justify-between w-full">
+            <h1 className="text-xl font-bold">Дополнительное</h1>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+            <IntegrationCard
+              title="Sentry"
+              description={"Подключение платформы для отслеживания ошибок и мониторинга приложений"}
+              dialog={<ConnectSentryDialog />}
+              testid={DATA_TEST_ID_DIALOG_CONNECT_SENTRY}
+            />
+            <IntegrationCard
+              title="Нужен сервис?"
+              description="Отправь заявку, а мы придумаем что-нибудь"
+              dialog={
+                <Link target="_blank" href={HREF_DISCORD}>
+                  <Button size="sm" variant="outline" className="w-fit">
+                    <LinkIcon className="mr-2" size={16} />
+                    Поддержка
+                  </Button>
+                </Link>
+              }
+            />
+          </div>
         </div>
       </div>
     </>
