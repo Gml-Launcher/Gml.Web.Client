92a16e7bf2c9d5ced66e241eb7551fa02a89f65d
akemiko <tcop29711@gmail.com>
2024-08-09 15:20:10 +0300

    Обновлена логика уведомлений
    Перенесена логика уведомлений в новое хранилище Zustand. Добавлена сортировка уведомлений и функция "Прочитать все". Также добавлен модуль для очистки уведомлений и улучшена работа с вебсокетами.

---
+++ src/views/notification/lib/store.ts
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import { NotificationBaseEntity } from "@/shared/api/contracts";
+
+interface Notifications {
+  count: number;
+  setCount: (i: number) => void;
+  addCount: () => void;
+  clearCount: () => void;
+
+  notifications: Array<NotificationBaseEntity>;
+  setNotifications: (notifys: NotificationBaseEntity[]) => void;
+  addNotification: (notify: NotificationBaseEntity) => void;
+  clearNotifications: () => void;
+}
+
+export const useNotificationsState = create<Notifications>()((set) => ({
+  count: 0,
+  setCount: (i: number) => {
+    set((state) => ({ count: state.count + i }));
+  },
+  addCount: () => {
+    set((state) => ({ count: state.count + 1 }));
+  },
+  clearCount: () => {
+    set(() => ({ count: 0 }));
+  },
+
+  notifications: [],
+  setNotifications: (notifys: NotificationBaseEntity[]) => {
+    set(() => ({ notifications: notifys }));
+  },
+  addNotification: (notify: NotificationBaseEntity) => {
+    set((state) => ({ notifications: state.notifications.concat(notify) }));
+  },
+  clearNotifications: () => {
+    set(() => ({ notifications: [] }));
+  },
+}));
---
+++ src/widgets/clear-notifications/index.ts
@@ -0,0 +1,1 @@
+export { ClearNotificationModel } from "./ui/ClearNotificationModel";
