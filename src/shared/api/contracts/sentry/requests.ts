61a800c49d66ee678ea13d676016fd7a612d81dc
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-09-09 16:45:24 +0300

    Добавлен виджет статистики Sentry
    Создан новый компонент SentryStats для отображения статистики ошибок с использованием данных из Sentry. Добавлены запросы для получения ошибок, исключений, статистики и сводки из Sentry API. Сущности SentryStats и SentrySummary внедрены в основную страницу с использованием хуков.

--- src/shared/api/contracts/sentry/requests.ts
+++ src/shared/api/contracts/sentry/requests.ts
@@ -1,13 +1,27 @@
-import {ResponseBaseEntity} from "@/shared/api/schemas";
-import {BaseSentryError, BaseSentryException} from "@/shared/api/contracts/sentry/schemas";
+import { ResponseBaseEntity } from "@/shared/api/schemas";
+import {
+  BaseSentryError,
+  BaseSentryException,
+  BaseSentryStats,
+  BaseSentrySummary,
+} from "@/shared/api/contracts/sentry/schemas";
 
-
-export type TGetSentryErrorsRequest = {}
+export type TGetSentryErrorsRequest = {};
 export type TGetSentryErrorsResponse = ResponseBaseEntity & {
-  data: BaseSentryError
-}
+  data: BaseSentryError;
+};
 
-export type TGetSentryExceptionRequest = {}
+export type TGetSentryExceptionRequest = {};
 export type TGetSentryExceptionResponse = ResponseBaseEntity & {
-  data: BaseSentryException
-}
+  data: BaseSentryException;
+};
+
+export type TGetSentryStatsRequest = {};
+export type TGetSentryStatsResponse = ResponseBaseEntity & {
+  data: BaseSentryStats[];
+};
+
+export type TGetSentrySummaryRequest = {};
+export type TGetSentrySummaryResponse = ResponseBaseEntity & {
+  data: BaseSentrySummary;
+};
--- src/views/sentry/ui/SentryPage.tsx
+++ src/views/sentry/ui/SentryPage.tsx
@@ -1,30 +1,27 @@
-"use client"
+"use client";
 
-import {DASHBOARD_PAGES} from "@/shared/routes";
-import {Breadcrumbs} from "@/shared/ui/Breadcrumbs";
-import {useSentryErrors} from "@/shared/hooks";
-import {SentryLoading} from "@/views/sentry/ui/SentryLoading";
-import {SentryChart} from "@/widgets/sentry-chart";
+import { DASHBOARD_PAGES } from "@/shared/routes";
+import { Breadcrumbs } from "@/shared/ui/Breadcrumbs";
+import { SentryStats } from "@/widgets/sentry-stats";
 
 export const SentryPage = () => {
-
-  const {data} = useSentryErrors()
-
-  if (!data) return <SentryLoading/>
-
   return (
     <>
       <Breadcrumbs
         current={"Sentry"}
         breadcrumbs={[{ value: "Главная", path: DASHBOARD_PAGES.HOME }]}
       />
-      <div className="flex gap-6 items-start">
-        {data.data.bugs.map(({graphics, exception}) => (
-          <div key={exception}>
-            <SentryChart graphics={graphics}/>
-          </div>
-        ))}
-      </div>
+
+      <SentryStats />
+
+      {/*<div className="flex gap-6 items-start">*/}
+      {/*  {data &&*/}
+      {/*    data.bugs.map(({ graphics, exception }) => (*/}
+      {/*      <div key={exception}>*/}
+      {/*        <SentryChart graphics={graphics} />*/}
+      {/*      </div>*/}
+      {/*    ))}*/}
+      {/*</div>*/}
     </>
   );
 };
