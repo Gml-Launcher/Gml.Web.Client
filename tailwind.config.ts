33ef2051a9aee7fa15ecb9b27e1023bd7db81c7f
terentev.a.a <terentev.a.a@recloud.tech>
2024-12-19 14:45:42 +0300

    Добавлены карточки с преимуществами на главную страницу
    Добавлены карточки с иконками и описанием функционала. Адаптированы стили, сетка и конфигурация Tailwind для улучшения пользовательского интерфейса. Также обновлены глобальные и Navbar стили.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,9 +14,13 @@ const config = {
     container: {
       center: true,
       padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1636px',
     },
     extend: {
       colors: {
