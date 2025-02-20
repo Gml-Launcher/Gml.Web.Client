38eae67e493a554856e77973c4c40b074fd71e8e
Terentev A. A. <terentev.a.a@recloud.tech>
2025-02-20 16:40:14 +0300

    Рефакторинг: вынес логику соцсетей в отдельный компонент
    Создан новый компонент SocialNetworkComponent для управления отдельными социальными сетями. Это позволяет улучшить читаемость и повторное использование кода в диалоге провайдера новостей.

--- src/widgets/news-provider-dialog/ui/NewsProviderDialog.tsx
+++ src/widgets/news-provider-dialog/ui/NewsProviderDialog.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import { PlugIcon } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link';
 
 import { Dialog, DialogContent, DialogTrigger } from '@/shared/ui/dialog';
 import { Button } from '@/shared/ui/button';
@@ -13,17 +12,13 @@ import { useActiveAuthIntegrations } from '@/shared/hooks';
 import vkLogo from '@/assets/logos/vk.svg';
 import telegramLogo from '@/assets/logos/telegram.svg';
 import websiteLogo from '@/assets/logos/website.svg';
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/shared/ui/card';
 import { AuthenticationType } from '@/shared/enums';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/shared/ui/card';
 import { Switch } from '@/shared/ui/switch';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '@/shared/ui/dropdown-menu';
-import { Input } from '@/shared/ui/input';
-import { FormLabel } from '@/shared/ui/form';
+  SocialNetwork,
+  SocialNetworkComponent,
+} from '@/widgets/news-provider-dialog/ui/SocialNetworkComponent';
 
 export function NewsProviderDialog() {
   const [open, setOpen] = useState(false);
@@ -37,7 +32,7 @@ export function NewsProviderDialog() {
     AuthenticationType.AUTHENTICATION_TYPE_DATALIFE_ENGINE,
   ];
 
-  const socialNetworks = [
+  const socialNetworks: SocialNetwork[] = [
     {
       name: 'Вконтакте',
       logo: vkLogo,
@@ -51,22 +46,15 @@ export function NewsProviderDialog() {
     {
       name: 'Telegram',
       logo: telegramLogo,
-      enabled: true,
+      enabled: false,
       description: 'Импорт новостей из социальной сети Telegram',
       descriptionToken: 'Введите токен Telegram бота',
       instructionLink: '/instructions/facebook',
     },
   ];
 
-  const enableNewsProvider = (social: {
-    name: string;
-    logo: any;
-    enabled: boolean;
-    description: string;
-    instructionLink: string;
-  }) => {
+  const enableNewsProvider = (social: SocialNetwork) => {
     social.enabled = true;
-    alert(social.enabled);
   };
 
   return (
@@ -85,86 +73,39 @@ export function NewsProviderDialog() {
               <TabsTrigger value="view">Предпросмотр</TabsTrigger>
             </TabsList>
             <TabsContent value="connect">
-              {isLoadingActiveIntegration && <div>Loading...</div>}
+              {isLoadingActiveIntegration ? (
+                <div>Loading...</div>
+              ) : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                  {socialNetworks &&
+                    socialNetworks.map((social) => (
+                      <SocialNetworkComponent social={social} key={social.name} />
+                    ))}
 
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {socialNetworks &&
-                  socialNetworks.map((social) => (
-                    <Card key={social.name}>
-                      <CardHeader className="flex flex-row items-center gap-5">
-                        <div className="flex items-center rounded-xl justify-center border min-h-12 min-w-12 h-12 w-12 p-3">
-                          <Image src={social.logo} className="w-full" alt={`${social.name} logo`} />
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          <CardTitle>{social.name}</CardTitle>
-                          <CardDescription>{social.description}</CardDescription>
-                        </div>
-                      </CardHeader>
-                      <hr />
-                      <CardFooter className="flex justify-between p-3">
+                  {/* Сайт */}
+                  {activeIntegrations &&
+                    includedNewsProvidersForCMS.includes(activeIntegrations?.authType) && (
+                      <Card>
+                        <CardHeader className="flex flex-row items-center gap-5">
+                          <div className="flex items-center rounded-xl justify-center border min-h-12 min-w-12 h-12 w-12 p-3">
+                            <Image src={websiteLogo} className="w-full" alt={'vk logo'} />
+                          </div>
+                          <div className="flex flex-col gap-2">
+                            <CardTitle>Сайт</CardTitle>
+                            <CardDescription>Импорт новостей из вашего сайта</CardDescription>
+                          </div>
+                        </CardHeader>
                         <hr />
-                        <div className="flex gap-5">
-                          <Link className="text-sm" target="_blank" href={social.instructionLink}>
-                            Инструкция
-                          </Link>
-                          <DropdownMenu>
-                            <DropdownMenuTrigger>
-                              <Switch checked={social.enabled} />
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent className="w-[300px]">
-                              <DropdownMenuLabel>Настройка</DropdownMenuLabel>
-                              <div className="p-1">
-                                <div className="flex justify-between items-center mb-1">
-                                  <FormLabel>Access Token</FormLabel>
-                                  {social.tokenGenerateUrl && (
-                                    <div className="flex items-center">
-                                      <Link
-                                        href={social.tokenGenerateUrl}
-                                        target="_blank"
-                                        className="underline text-sm"
-                                      >
-                                        Получить токен
-                                      </Link>
-                                    </div>
-                                  )}
-                                </div>
-                                <Input type="text" placeholder={social.descriptionToken} />
-                                <Button
-                                  variant="secondary"
-                                  className="w-full mt-2"
-                                  onClick={() => enableNewsProvider(social)}
-                                >
-                                  Включить
-                                </Button>
-                              </div>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </div>
-                      </CardFooter>
-                    </Card>
-                  ))}
-                {activeIntegrations &&
-                  includedNewsProvidersForCMS.includes(activeIntegrations?.authType) && (
-                    <Card>
-                      <CardHeader className="flex flex-row items-center gap-5">
-                        <div className="flex items-center rounded-xl justify-center border min-h-12 min-w-12 h-12 w-12 p-3">
-                          <Image src={websiteLogo} className="w-full" alt={'vk logo'} />
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          <CardTitle>Сайт</CardTitle>
-                          <CardDescription>Импорт новостей из вашего сайта</CardDescription>
-                        </div>
-                      </CardHeader>
-                      <hr />
-                      <CardFooter className="flex justify-between p-3">
-                        <hr />
-                        <div className="flex gap-5">
-                          <Switch />
-                        </div>
-                      </CardFooter>
-                    </Card>
-                  )}
-              </div>
+                        <CardFooter className="flex justify-between p-3">
+                          <hr />
+                          <div className="flex gap-5">
+                            <Switch />
+                          </div>
+                        </CardFooter>
+                      </Card>
+                    )}
+                </div>
+              )}
             </TabsContent>
             <TabsContent value="view"></TabsContent>
           </Tabs>
---
+++ src/widgets/news-provider-dialog/ui/SocialNetworkComponent.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { MoveUpRight } from 'lucide-react';
+
+import { Button } from '@/shared/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/shared/ui/card';
+import { Switch } from '@/shared/ui/switch';
+import { Input } from '@/shared/ui/input';
+
+export interface SocialNetwork {
+  name: string;
+  logo: any;
+  enabled: boolean;
+  description: string;
+  descriptionToken: string;
+  tokenGenerateUrl?: string;
+  instructionLink?: string;
+}
+
+export function SocialNetworkComponent({ social }: { social: SocialNetwork }) {
+  const [enabled, setEnabled] = useState(social.enabled);
+  const onOpenChange = () => setEnabled((prev) => !prev);
+
+  return (
+    <Card key={social.name}>
+      <CardHeader className="flex flex-row items-center relative gap-5">
+        <div className="flex items-center rounded-xl justify-center border min-h-12 min-w-12 h-12 w-12 p-3">
+          <Image src={social.logo} className="w-full" alt={`${social.name} logo`} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <CardTitle>{social.name}</CardTitle>
+          <CardDescription>{social.description}</CardDescription>
+        </div>
+        <Switch
+          defaultChecked={enabled}
+          onCheckedChange={onOpenChange}
+          className="absolute top-2 right-3"
+        />
+      </CardHeader>
+      {enabled && (
+        <>
+          <hr />
+          <CardFooter>
+            <div className="flex flex-col pt-6 w-full gap-1">
+              <Input type="text" placeholder={social.descriptionToken} />
+              <Button variant="secondary" className="w-full mt-2">
+                Сохранить
+              </Button>
+              <div className="flex justify-center mt-2">
+                {social.tokenGenerateUrl && (
+                  <div className="flex duration-300 items-center text-muted-foreground hover:text-white">
+                    <Link
+                      href={social.tokenGenerateUrl}
+                      target="_blank"
+                      className="underline text-sm flex items-end"
+                    >
+                      Получить токен
+                      <MoveUpRight size={16} className="ml-1" />
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </div>
+          </CardFooter>
+        </>
+      )}
+    </Card>
+  );
+}
