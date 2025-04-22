9864ad02ee52bec38a65bf235d6d839428493ee5
Terentev A. A. <terentev.a.a@recloud.tech>
2025-04-22 15:50:56 +0300

    Добавить компонент Accordion и обновить зависимости
    Добавлен компонент Accordion на основе библиотеки @radix-ui/react-accordion. Также обновлены package-lock.json и связанные зависимости, чтобы поддерживать работу нового функционала.

--- src/widgets/sentry-analytics/ui/SentryAnalytics.tsx
+++ src/widgets/sentry-analytics/ui/SentryAnalytics.tsx
@@ -12,8 +12,17 @@ import {
 import { DateRange } from 'react-day-picker';
 import { CopyIcon } from '@radix-ui/react-icons';
 import { toast } from 'sonner';
+import { MemoryStick, Monitor, ReceiptText } from 'lucide-react';
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/shared/ui/card';
+import { Progress } from '@/shared/ui/progress';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/shared/ui/card';
 import { Tabs, TabsList, TabsTrigger } from '@/shared/ui/tabs';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/shared/ui/table';
 import { Separator } from '@/shared/ui/separator';
@@ -24,6 +33,12 @@ import { DatePickerWithRange } from '@/shared/ui/data-range-picker';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/shared/ui/select';
 import { Skeleton } from '@/shared/ui/skeleton';
 import { Button } from '@/shared/ui/button';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/shared/ui/accordion';
 
 export const SentryAnalytics = () => {
   const [date, setDate] = useState<DateRange | undefined>(undefined);
@@ -48,7 +63,6 @@ export const SentryAnalytics = () => {
 
     const dataFromYear = startOfYear(todayDate);
     const dataToYear = endOfYear(todayDate);
-
 
     switch (tab) {
       case AnalyticsInterval.ANALYTICS_INTERVAL_FIVE_MINUTES: {
@@ -131,7 +145,7 @@ export const SentryAnalytics = () => {
 
   const copyText = async (content: string) => {
     try {
-      await navigator.clipboard.writeText(bug.stackTrace);
+      await navigator.clipboard.writeText(content);
       toast('Текст успешно скопирован', {
         duration: 500,
         onAutoClose: () => true,
@@ -142,7 +156,7 @@ export const SentryAnalytics = () => {
         duration: 2000,
       });
     }
-  }
+  };
 
   return (
     <>
@@ -218,33 +232,133 @@ export const SentryAnalytics = () => {
                       <TableHead>Ошибка</TableHead>
                       <TableHead>У скольки пользователей</TableHead>
                       <TableHead>Количество повторений</TableHead>
-                      <TableHead>Действия</TableHead>
                       {/*<TableHead>График</TableHead>*/}
                     </TableRow>
                   </TableHeader>
                   <TableBody>
                     {data &&
                       data.data.data.map((bug) => {
+                        // @ts-ignore
                         return (
                           <TableRow key={bug.exception}>
                             <TableCell>
-                              {bug.exception}
-                              <p className="text-sm text-gray-700 dark:text-gray-400 text-wrap">
-                                {bug.stackTrace.split('\n')[0]}
-                              </p>
+                              <Accordion type="single" collapsible>
+                                <AccordionItem value="item-1" className="border-none">
+                                  <AccordionTrigger className="relative">
+                                    <div className="flex gap-3">
+                                      <Button
+                                        className="p-0 min-h-[35px] min-w-[35px] h-[35px] w-[35px]"
+                                        onClick={() => void copyText(bug.stackTrace.split('\n')[0])}
+                                        variant="ghost"
+                                      >
+                                        <CopyIcon />
+                                      </Button>
+
+                                      <div className="flex flex-col">
+                                        {bug.exception}
+                                        <p className="text-sm relative text-gray-700 rounded-lg dark:text-gray-400 text-wrap">
+                                          {bug.stackTrace.split('\n')[0]}
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </AccordionTrigger>
+                                  <AccordionContent>
+                                    <div className="flex flex-col md:flex-row gap-3 ml-[45px]">
+                                      <Card className="w-full md:w-[50%]">
+                                        <CardHeader className="pb-0 text-gray-700 dark:text-gray-400">
+                                          <Monitor size={24} className="mb-3" />
+                                          Имя устройства
+                                        </CardHeader>
+                                        <CardFooter>
+                                          {bug.bugInfo.username} / {bug.bugInfo.pcName} -{' '}
+                                          {bug.bugInfo.osIdentifier} {bug.bugInfo.osVersion}
+                                        </CardFooter>
+                                      </Card>
+                                      <Card className="w-full md:w-[50%]">
+                                        <CardHeader className="pb-0 text-gray-700 dark:text-gray-400">
+                                          <MemoryStick size={24} className="mb-3" />
+                                          Зарезервированная память
+                                        </CardHeader>
+                                        <CardFooter className="gap-3">
+                                          {(bug.bugInfo.memoryInfo.totalAvailableMemoryBytes === 0
+                                            ? 0
+                                            : (bug.bugInfo.memoryInfo.allocatedBytes /
+                                                bug.bugInfo.memoryInfo.totalAvailableMemoryBytes) *
+                                              100
+                                          ).toFixed(2)}
+                                          %
+                                          <div className="relative h-2 mt-1 w-full">
+                                            <Progress
+                                              value={
+                                                (bug.bugInfo.memoryInfo.allocatedBytes /
+                                                  bug.bugInfo.memoryInfo
+                                                    .totalAvailableMemoryBytes) *
+                                                100
+                                              }
+                                              className={`w-full h-1 ${
+                                                (bug.bugInfo.memoryInfo.allocatedBytes /
+                                                  bug.bugInfo.memoryInfo
+                                                    .totalAvailableMemoryBytes) *
+                                                  100 >
+                                                80
+                                                  ? 'bg-red-500'
+                                                  : (bug.bugInfo.memoryInfo.allocatedBytes /
+                                                        bug.bugInfo.memoryInfo
+                                                          .totalAvailableMemoryBytes) *
+                                                        100 >
+                                                      60
+                                                    ? 'bg-yellow-500'
+                                                    : 'bg-green-500'
+                                              }`}
+                                            />
+                                          </div>
+                                        </CardFooter>
+                                      </Card>
+                                      <Card className="w-full md:w-[50%]">
+                                        <CardHeader className="pb-0 text-gray-700 dark:text-gray-400">
+                                          <ReceiptText size={24} className="mb-3" />
+                                          Детали
+                                        </CardHeader>
+                                        <CardFooter>
+                                          <div className="flex flex-col gap-2">
+                                            <p>
+                                              Адрес:
+                                              {bug.bugInfo.ipAddress}
+                                            </p>
+                                            <p>
+                                              Вызвало краш:
+                                              <span
+                                                className={`text-lg ${!bug.bugInfo.exceptions[0].crashed ? 'text-red-500' : 'text-green-500'}`}
+                                              >
+                                                {!bug.bugInfo.exceptions[0].crashed ? '✗' : '✓'}
+                                              </span>
+                                            </p>
+                                            <p>
+                                              Текущее приложение:
+                                              <span
+                                                className={`text-lg ${bug.bugInfo.exceptions[0].current ? 'text-red-500' : 'text-green-500'}`}
+                                              >
+                                                {bug.bugInfo.exceptions[0].current ? '✗' : '✓'}
+                                              </span>
+                                            </p>
+                                          </div>
+                                        </CardFooter>
+                                      </Card>
+                                    </div>
+                                    <Button
+                                      className="mt-3 ml-[45px]"
+                                      onClick={() => void copyText(bug.stackTrace)}
+                                      variant="secondary"
+                                    >
+                                      <CopyIcon />
+                                      Копировать всё
+                                    </Button>
+                                  </AccordionContent>
+                                </AccordionItem>
+                              </Accordion>
                             </TableCell>
                             <TableCell width="110">{bug.countUsers}</TableCell>
                             <TableCell width="110">{bug.count}</TableCell>
-                            <TableCell width="110">
-                              <Button
-                                className="gap-3"
-                                onClick={copyText(bug.stackTrace)}
-                                variant="secondary"
-                              >
-                                <CopyIcon />
-                                Копировать всё
-                              </Button>
-                            </TableCell>
                           </TableRow>
                         );
                       })}
